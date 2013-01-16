b1c3d8c967040d782bb4d1b9417d3eb6fc21b6a9
The Hackmaster <julian@workzeit.com>
2013-01-15 20:55:12 -0500

    adding load average script

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-blink1-python
-=============
+= blink(1)-python
 
-Python libraries and scripts for ThingM's awesome blink(1).
+== Ooh, what do I get?
+
+  - __blink1-loadavg.py__: A load average visualizer - fun to watch it slowly glow red as a runaway task starts to kick in
+  - More to come...
+
+== How do I use it?
+
+  - For now, blink1-loadavg will only work on Linux. Make sure you have blink1-tool in your path (and in my case, setuid root)
+  	- Be sure to change the __NUM_CORES__ value to match the number of CPU cores in your system for extra awesomeness.
+
+== In the works
+
+  - A full python library for Linux/OSX/Win that will support (at minimum) all the features of blink1-tool
---
+++ blink1-loadavg.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import re
+from subprocess import call
+from subprocess import check_output
+from time import sleep
+
+# Change this to the number of CPU cores (not physical or virtual processors)
+# in your system. Added because 1.0 load on a multicore system does not
+# actually mean 100% CPU load.
+NUM_CORES = 4
+
+LOAD_RE = re.compile('.+average:\s(\d+\.\d+),\s(\d+\.\d+),\s(\d+\.\d+)')
+
+if __name__ == '__main__':
+    print 'Modulating blink(1) to the load averages...'
+    print ''
+    print 'Press Ctrl+C to quit.'
+    try:
+        while True:
+            load_averages = LOAD_RE.match(check_output(['uptime'])).groups()
+            one, five, fifteen = [(float(l) / NUM_CORES) * 255 for l in load_averages]
+            params = ['blink1-tool', '-q']
+            if one > 255 or five > 255 or fifteen > 255:
+                m = max(one, five, fifteen)
+                one *= 255 / m
+                five *= 255 / m
+                fifteen *= 255 / m
+                rgb_val = '{0:.0f},{1:.0f},{2:.0f}'.format(one, five, fifteen)
+                params += ['--rgb', rgb_val]
+                if one > 1275 or five > 1275 or fifteen > 1275:
+                    params += ['-m', '250', '--blink', '20']
+                else:
+                    params += ['-m', '500', '--blink', '10']
+                call(params)
+            else:
+                rgb_val = '{0:.0f},{1:.0f},{2:.0f}'.format(one, five, fifteen)
+                params += ['-m', '5000', '--rgb', rgb_val]
+                call(params)
+                sleep(5)
+    except KeyboardInterrupt:
+        pass
